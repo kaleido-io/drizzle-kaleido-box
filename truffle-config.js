a71b93e33bb55e4e03b50492f2c266e3e8fa0510
Josh Mobley <joshua.mobley@consensys.net>
2019-04-03 14:55:13 -0400

    initial commit of drizzle box

---
+++ truffle-config.js
@@ -0,0 +1,21 @@
+const path = require("path");
+var Web3 = require('web3');
+
+module.exports = {
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // to customize your Truffle configuration!
+  contracts_build_directory: path.join(__dirname, "app/src/contracts"),
+  networks: {
+    development: {
+      provider: () => {
+        const appCred = 'myAppCredential'; // from application credential widget
+        const connectionURL = 'myConnectionURL'; // without protocol (https://)
+        return new Web3.providers.HttpProvider(`https://${appCred}@${connectionURL}`, 100000);
+      },
+      network_id: "*", // Match any network id
+      gasPrice: 0,
+      gas: 4500000,
+      /* type: 'quorum' // Use this property for Quorum environments */
+    },
+  }
+};
