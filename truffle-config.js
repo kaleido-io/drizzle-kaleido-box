a0f3eee12c74ce76c7e01f9abce035d4a76e3aae
jimthematrix <jim_the_matrix@hotmail.com>
2022-11-01 13:29:35 -0400

    Update dependencies to fix npm issues (#32)
    * Update dependencies to fix npm issues
    
    * Use rate-limit aware http client
    
    * Update package version
    
    * Fix Drizzle import
    
    * update react packages to work with @drizzle/store
    
    Co-authored-by: jebonfig <joe.bonfiglio@kaleido.io>

--- truffle-config.js
+++ truffle-config.js
@@ -1,26 +1,30 @@
-const path = require("path");
-var Web3 = require('web3');
+const HTTPProviderRateLimitRetry = require('./lib/http-provider-rate-limit-retry');
+const path = require('path');
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
-  contracts_build_directory: path.join(__dirname, "app/src/contracts"),
+  contracts_build_directory: path.join(__dirname, 'app/src/contracts'),
   networks: {
     development: {
       provider: () => {
-        const appCred = 'yourappcred'; // from application credential widget
-        const connectionURL = 'nodeConnectionURL'; // without protocol (https://)
-        return new Web3.providers.HttpProvider(`https://${appCred}@${connectionURL}`, 100000);
+        // JSON/RPC connection URL including the app creds:
+        // eg: https://[username]:[password]@[rpc-endpoint-hostname]
+        const connectionURL = 'nodeConnectionURL';
+        return new HTTPProviderRateLimitRetry(connectionURL, 100000);
       },
-      network_id: "*", // Match any network id
+      network_id: '*', // Match any network id
       gasPrice: 0,
       gas: 4500000,
+      disableConfirmationListener: true, // generates thousands of eth_getBlockByNumber calls
+      timeoutBlocks: 3,
+      deploymentPollingInterval: 5000,
       /* type: 'quorum' // Use this property for Quorum environments */
     },
   },
   compilers: {
     solc: {
-      version: "0.5.0",
-    }
-  }
+      version: '0.5.0',
+    },
+  },
 };
