34060fcb3f847babfb88c95fd6ba85bebbe46e69
Jim Zhang <jim.zhang@kaleido.io>
2022-11-01 12:35:17 -0400

    Fix Drizzle import

--- app/src/index.js
+++ app/src/index.js
@@ -4,16 +4,16 @@ import './index.css';
 import App from './App';
 
 // import drizzle functions and contract artifact
-import { Drizzle } from "drizzle";
-import MyStringStore from "./contracts/SimpleStorage.json";
+import { Drizzle } from '@drizzle/store';
+import MyStringStore from './contracts/SimpleStorage.json';
 
 // let drizzle know what contracts we want and how to access our test blockchain
 const options = {
   contracts: [MyStringStore],
   web3: {
     fallback: {
-      type: "ws",
-      url: "ws://127.0.0.1:7545",
+      type: 'ws',
+      url: 'ws://127.0.0.1:7545',
     },
   },
 };
